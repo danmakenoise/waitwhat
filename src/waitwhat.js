3743efd7fe8f96e221df615271aafdfc87894652
Dan Phillips <danmakenoise@gmail.com>
2017-03-31 11:04:07 -0400

    define context object properly

--- src/waitwhat.js
+++ src/waitwhat.js
@@ -1,7 +1,15 @@
 import repl from 'repl';
 
 const waitWhat = (context) => {
-  repl.start().context = context;
+  const r = repl.start('ಠ_ಠ > ');
+
+  Object.keys(context).forEach((key) => {
+    Object.defineProperty(r.context, key, {
+      configurable: false,
+      enumerable: true,
+      value: context[key],
+    });
+  });
 };
 
 export default waitWhat;
